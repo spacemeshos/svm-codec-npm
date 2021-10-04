4674960dc20a94d9e5c2073797ab1afae6c70e8d
Aviv Eyal <aviveyal07@gmail.com>
2021-10-04 14:03:23 +0300

    major refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-const assert = require("assert");
-
 export const OK_MARKER = 1;
 export const ERR_MARKER = 0;
 
@@ -12,107 +10,103 @@ export async function init(code: BufferSource) {
     codec = await WebAssembly.instantiate(wasm, {})
 }
 
+// Returns true iff library was initialized with wasm code
 export function isInitialized() {
     return (codec !== null)
 }
 
-// Call an svm_codec function with the provided buffer. Returns result buffer.
-export function call(funcName: string, buf) {
-    return (codec!.exports as any)[funcName](buf)
+export interface DeployData {
+    svm_version: number,
+    code_version: number,
+    name: string,
+    desc: string,
+    code: string,
+    data: string,
+    ctors: Array<string>
 }
 
-// Allocates a svm_codec buffer with the provided byte length
-export function wasmBufferAlloc(length: number) {
-    return (codec!.exports as any).wasm_alloc(length)
+export interface SpawnData {
+    version: number,
+    template: string,
+    name: string
+    ctor_name: string,
+    calldata: Uint8Array,
 }
 
-// Frees an allocated svm_codec buffer that was previously allocated by svm_codec
-export function wasmBufferFree(buf) {
-    return (codec!.exports as any).wasm_free(buf);
+export interface CallData {
+    version: number,
+    target: string,
+    func_name: string,
+    verifydata: string,
+    calldata: Uint8Array,
 }
 
-// Returns the bytes length of a wasm_codec buffer
-export function wasmBufferLength(buf) {
-    return (codec!.exports as any).wasm_buffer_length(buf);
-}
+export function encodeDeployData(data: DeployData) : Uint8Array {
+    const buf = newWasmBuffer(data);
+    const result = call("wasm_encode_deploy", buf);
+    const len = wasmBufferLength(result);
+    const slice = wasmBufferDataSlice(result, 0, len);
 
-// Frees the data allocated in a svm_codec buffer
-export function wasmBufferDataPtr(buf) {
-    return (codec!.exports as any).wasm_buffer_data(buf);
-}
-
-// Copies binary data from buf to an svm_codec memory buffer
-export function copyToWasmBufferData(buf, data) {
-    let ptr = wasmBufferDataPtr(buf);
-    let memory = (codec!.exports as any).memory.buffer;
-    let view = new Uint8Array(memory);
-    view.set([...data], ptr);
-}
-
-// Copies length bytes at an offset from buf to an svm_codec memory buffer
-export function wasmBufferDataSlice(buf, offset, length: number) {
-    let ptr = wasmBufferDataPtr(buf);
-    const memory = (codec!.exports as any).memory.buffer;
-    const view = new Uint8Array(memory);
-    return view.slice(ptr + offset, ptr + offset + length);
-}
-
-// Creates a wasm buffer that can be passed to an svm_codec instance methods from a provided json object
-// Returns the buffer
-export function newWasmBuffer(object: JSON) {
-    const objectStr = JSON.stringify(object);
-    const bytes = new TextEncoder().encode(objectStr);
-    const buf = wasmBufferAlloc(bytes.length);
-    assert.strictEqual(bytes.length, wasmBufferLength(buf));
-    copyToWasmBufferData(buf, bytes);
-    return buf;
-}
-
-// Returns a json object of a provided wasm buffer
-export function loadWasmBuffer(buf) : JSON {
-    let length = wasmBufferLength(buf);
-    const slice = wasmBufferDataSlice(buf, 0, length);
-    const string = new TextDecoder().decode(slice);
-    return JSON.parse(string);
-}
-
-// Returns a utf-8 string representation of the data in an svm_codec buffer
-function loadWasmBufferDataAsString(buf) : string {
-    let length = wasmBufferLength(buf);
-    const slice = wasmBufferDataSlice(buf, 0, length);
-    assert.strictEqual(slice[0], OK_MARKER);
-    return new TextDecoder().decode(slice.slice(1));
-}
-
-// Returns a json object representation of the data in a svm_codec buffer
-// Throws an exception if buffer has an error with the exception's string representation
-export function loadWasmBufferDataAsJson(buf) : JSON {
-    let length = wasmBufferLength(buf);
-    const slice = wasmBufferDataSlice(buf, 0, length);
-
-    if (slice[0] === ERR_MARKER) {
-        const msg = loadWasmBufferError(buf);
-        console.warn(msg);
-        throw msg;
+    if (slice[0] !== OK_MARKER) {
+        throw loadWasmBufferError(result);
     }
 
-    assert.strictEqual(slice[0], OK_MARKER);
-
-    const string = loadWasmBufferDataAsString(buf);
-
-    return JSON.parse(string);
+    wasmBufferFree(buf);
+    wasmBufferFree(result);
+    return slice.slice(1);
 }
 
-// Returns a utf-8 string representation of an error in an svm_codec buffer
-export function loadWasmBufferError(buf) {
-    let length = wasmBufferLength(buf);
-    const slice = wasmBufferDataSlice(buf, 0, length);
-    // assert.strictEqual(slice[0], ERR_MARKER);
-    return new TextDecoder().decode(slice.slice(1));
+export function encodeSpawnData(data: SpawnData) : Uint8Array {
+    const buf = newWasmBuffer(data);
+    const result = call("wasm_encode_spawn", buf);
+    const len = wasmBufferLength(result);
+    const slice = wasmBufferDataSlice(result, 0, len);
+
+    if (slice[0] !== OK_MARKER) {
+        throw loadWasmBufferError(result);
+    }
+
+    wasmBufferFree(buf);
+    wasmBufferFree(result);
+    return slice.slice(1);
+}
+
+export function decodeSpawnData(data: Uint8Array) : JSON {
+    const buf = newWasmBuffer({data: binToString(data)});
+    const result = call("wasm_decode_spawn", buf);
+    const json = loadWasmBufferDataAsJson(result);
+    wasmBufferFree(buf);
+    wasmBufferFree(result);
+    return json;
+}
+
+export function encodeCallData(data:CallData) : Uint8Array {
+    const buf = newWasmBuffer(data);
+    const result = call("wasm_encode_call", buf);
+    const len = wasmBufferLength(result);
+    const slice = wasmBufferDataSlice(result, 0, len);
+
+    if (slice[0] !== OK_MARKER) {
+        throw loadWasmBufferError(result);
+    }
+
+    wasmBufferFree(buf);
+    wasmBufferFree(result);
+    return slice.slice(1);
+}
+
+export function decodeCallData(bytes: Uint8Array): JSON {
+    const data = binToString(bytes);
+    const buf = newWasmBuffer({ data: data });
+    const result = call("wasm_decode_call", buf);
+    const json = loadWasmBufferDataAsJson(result);
+    wasmBufferFree(buf);
+    wasmBufferFree(result);
+    return json;
 }
 
 // Encodes data provided in josn object and returns the encoded data in a json object
-export function encodeInput(object) : JSON {
+export function encodeInput(object: any) : JSON {
     const buf = newWasmBuffer(object);
     const result = call("wasm_encode_inputdata", buf);
     const encoded = loadWasmBufferDataAsJson(result);
@@ -130,3 +124,128 @@ export function decodeInput(encodedData) : JSON {
     wasmBufferFree(result);
     return json;
 }
+
+// Encodes binary data provided in array as a hex binary string (without an 0x prefix)
+export function binToString(array: Uint8Array) : string {
+    let result = "";
+
+    for (const b of array) {
+        // toString takes no arg????
+        let s = b.toString(16);
+
+        // padding
+        if (s.length < 2) {
+            s = "0" + s;
+        }
+        result += s;
+    }
+
+    return result;
+}
+
+
+///// Internal help functions below
+
+
+// Call an svm_codec function with the provided buffer. Returns result buffer.
+function call(funcName: string, buf) {
+    return (codec!.exports as any)[funcName](buf)
+}
+
+// Allocates a svm_codec buffer with the provided byte length
+function wasmBufferAlloc(length: number) {
+    return (codec!.exports as any).wasm_alloc(length)
+}
+
+// Frees an allocated svm_codec buffer that was previously allocated by svm_codec
+function wasmBufferFree(buf) {
+    return (codec!.exports as any).wasm_free(buf);
+}
+
+// Returns the bytes length of a wasm_codec buffer
+function wasmBufferLength(buf) {
+    return (codec!.exports as any).wasm_buffer_length(buf);
+}
+
+// Frees the data allocated in a svm_codec buffer
+function wasmBufferDataPtr(buf) {
+    return (codec!.exports as any).wasm_buffer_data(buf);
+}
+
+// Copies binary data from buf to an svm_codec memory buffer
+function copyToWasmBufferData(buf, data) {
+    let ptr = wasmBufferDataPtr(buf);
+    let memory = (codec!.exports as any).memory.buffer;
+    let view = new Uint8Array(memory);
+    view.set([...data], ptr);
+}
+
+// Copies length bytes at an offset from buf to an svm_codec memory buffer
+function wasmBufferDataSlice(buf, offset, length: number) {
+    let ptr = wasmBufferDataPtr(buf);
+    const memory = (codec!.exports as any).memory.buffer;
+    const view = new Uint8Array(memory);
+    return view.slice(ptr + offset, ptr + offset + length);
+}
+
+// Creates a wasm buffer that can be passed to an svm_codec instance methods from a provided json object
+// Returns the buffer
+function newWasmBuffer(object: any) {
+    const objectStr = JSON.stringify(object);
+    const bytes = new TextEncoder().encode(objectStr);
+    const buf = wasmBufferAlloc(bytes.length);
+
+    if (bytes.length !== wasmBufferLength(buf)) {
+        throw new Error("svm codec error: unexpected buf length")
+    }
+
+    copyToWasmBufferData(buf, bytes);
+    return buf;
+}
+
+/*
+// Returns a json object of a provided wasm buffer
+function loadWasmBuffer(buf) : any {
+    let length = wasmBufferLength(buf);
+    const slice = wasmBufferDataSlice(buf, 0, length);
+    const string = new TextDecoder().decode(slice);
+    return JSON.parse(string);
+}*/
+
+
+// Returns a utf-8 string representation of the data in an svm_codec buffer
+function loadWasmBufferDataAsString(buf) : string {
+    let length = wasmBufferLength(buf);
+    const slice = wasmBufferDataSlice(buf, 0, length);
+    if (slice[0] !== OK_MARKER) {
+        throw loadWasmBufferError(buf)
+    }
+
+    return new TextDecoder().decode(slice.slice(1));
+}
+
+
+// Returns a json object representation of the data in a svm_codec buffer
+// Throws an exception if buffer has an error with the exception's string representation
+function loadWasmBufferDataAsJson(buf) : JSON {
+    let length = wasmBufferLength(buf);
+    const slice = wasmBufferDataSlice(buf, 0, length);
+
+    if (slice[0] === ERR_MARKER) {
+        throw loadWasmBufferError(buf);
+    }
+
+    const string = loadWasmBufferDataAsString(buf);
+
+    return JSON.parse(string);
+}
+
+// Returns a utf-8 string representation of an error in an svm_codec buffer
+function loadWasmBufferError(buf) {
+    let length = wasmBufferLength(buf);
+    const slice = wasmBufferDataSlice(buf, 0, length);
+    // assert.strictEqual(slice[0], ERR_MARKER);
+    return new TextDecoder().decode(slice.slice(1));
+}
+
+
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,6 +1,12 @@
 //import * as SvmCodec from "../dist";
 
-const SvmCodec = require("../dist");
+// import {DeployData, encodeDeploy} from "../src";
+
+//import {DeployData, SpawnData, CallData,} from "../dist";
+
+import * as SvmCodec from "../dist";
+
+//const SvmCodec = require("../dist");
 
 // test helper functions
 
@@ -17,22 +23,6 @@ function generateAddress(s: string) : string {
     return repeatString(s, 20);
 }
 
-// Encodes binary data provided in array as a hex binary string (without an 0x prefix)
-function binToString(array: Uint8Array) : string {
-    let result = "";
-    array.forEach((b) => {
-        // toString takes no arg????
-        let s = b.toString(16);
-
-        // padding
-        if (s.length < 2) {
-            s = "0" + s;
-        }
-        result += s;
-    });
-
-    return result;
-}
 
 async function init() {
     const fs = require('fs');
@@ -52,6 +42,7 @@ describe("SvmCodec Lib", () => {
 });
 
 
+/*
 describe("WASM Buffer", () => {
     it("Allocate & free", async () => {
         await init()
@@ -68,7 +59,7 @@ describe("WASM Buffer", () => {
 
         //expect(sum(1, 1)).toEqual(2);
     });
-});
+});*/
 
 
 //////////////
@@ -132,7 +123,7 @@ describe("Deploy Template", function () {
     it("Encodes & Decodes valid transactions", async function () {
         await init();
 
-        const tx = {
+        const data : SvmCodec.DeployData = {
             svm_version: 1,
             code_version: 2,
             name: "My Template",
@@ -142,35 +133,15 @@ describe("Deploy Template", function () {
             ctors: ["init", "start"],
         };
 
-        const buf = SvmCodec.newWasmBuffer(tx);
-        const result = SvmCodec.call("wasm_encode_deploy", buf);
-        const len = SvmCodec.wasmBufferLength(result);
-        const slice = SvmCodec.wasmBufferDataSlice(result, 0, len);
-        expect(slice[0]).toStrictEqual(SvmCodec.OK_MARKER);
+        SvmCodec.encodeDeployData(data);
 
-        // `bytes` is a `Uint8Array` holding the encoded `SVM spawn-account` transaction
-        //const bytes = slice.slice(1);
-
-        SvmCodec.wasmBufferFree(buf);
-        SvmCodec.wasmBufferFree(result);
     });
 
     it("Handles errors for invalid transactions", async function () {
 
         await init();
-        const tx = {
-            svm_version: 1,
-            code_version: 2,
-        };
 
-        const buf = SvmCodec.newWasmBuffer(tx);
-        const result = SvmCodec.call("wasm_encode_deploy", buf);
-
-        const error = SvmCodec.loadWasmBufferError(result);
-        expect(error).toStrictEqual("A non-optional field was missing (`name`).");
-
-        SvmCodec.wasmBufferFree(buf);
-        SvmCodec.wasmBufferFree(result);
+        // todo: what will be invalid DeployData data input?
     });
 });
 
@@ -179,33 +150,17 @@ describe("Deploy Template", function () {
 describe("Spawn Account", function () {
 
     /// spawn helper
-    function encodeSpawn(template: string, name: string, calldata: Uint8Array) : Uint8Array {
-        const tx = {
+    function encodeSpawnData(template: string, name: string, callData: Uint8Array) : Uint8Array {
+        const data : SvmCodec.SpawnData = {
             version: 0,
             template: template,
             name: name,
             ctor_name: "initialize",
-            calldata: calldata,
+            calldata: callData
         };
-        const buf = SvmCodec.newWasmBuffer(tx);
-        const result = SvmCodec.call("wasm_encode_spawn", buf);
-        const len = SvmCodec.wasmBufferLength(result);
-        const slice = SvmCodec.wasmBufferDataSlice(result, 0, len);
-        expect(slice[0]).toStrictEqual(SvmCodec.OK_MARKER);
-        SvmCodec.wasmBufferFree(buf);
-        SvmCodec.wasmBufferFree(result);
-        return slice.slice(1);
-    }
 
-    // Span helper
-    function decodeSpawn(bytes: Uint8Array) : JSON {
-        const data = binToString(bytes);
-        const buf = SvmCodec.newWasmBuffer({ data: data });
-        const result = SvmCodec.call("wasm_decode_spawn", buf);
-        const json = SvmCodec.loadWasmBufferDataAsJson(result);
-        SvmCodec.wasmBufferFree(buf);
-        SvmCodec.wasmBufferFree(result);
-        return json;
+        return SvmCodec.encodeSpawnData(data);
+
     }
 
     it("Encodes & Decodes valid transactions", async function () {
@@ -216,9 +171,9 @@ describe("Spawn Account", function () {
             abi: ["i32", "i64"],
             data: [10, 20],
         };
-        const calldata: JSON = SvmCodec.encodeInput(object);
-        const bytes = encodeSpawn(template, name, calldata["data"]);
-        const json = decodeSpawn(bytes);
+        const callData: JSON = SvmCodec.encodeInput(object);
+        const bytes = encodeSpawnData(template, name, callData["data"])
+        const json = SvmCodec.decodeSpawnData(bytes);
         expect(json).toStrictEqual({
             version: 0,
             template: template,
@@ -234,79 +189,54 @@ describe("Spawn Account", function () {
     it("Handles errors for invalid transactions", async function () {
         await init();
 
-        const tx = {
-            version: 0,
-            template: "102030",
-        };
+        expect(() => encodeSpawnData("102030", "", new Uint8Array(0)))
+            .toThrow("The value of a specific field is invalid (`template`).");
 
-        const buf = SvmCodec.newWasmBuffer(tx);
-        const result = SvmCodec.call("wasm_encode_spawn", buf);
-        const error = SvmCodec.loadWasmBufferError(result);
+        /*
         expect(error).toStrictEqual(
             "The value of a specific field is invalid (`template`)."
-        );
-
-        SvmCodec.wasmBufferFree(buf);
-        SvmCodec.wasmBufferFree(result);
-
+        );*/
     });
 });
 
 ////////////////
 
 describe("Call Account", function () {
-    function encodeCall(target: string, verifydata: Uint8Array, calldata: Uint8Array) : Uint8Array {
-        const tx = {
+    function encodeCallData(target: string, verifyData: Uint8Array, callData: Uint8Array) : Uint8Array {
+        const data : SvmCodec.CallData = {
             version: 0,
             target: target,
             func_name: "do_something",
-            verifydata: verifydata,
-            calldata: calldata,
+            verifydata: SvmCodec.binToString(verifyData),
+            calldata: callData,
         };
-
-        const buf = SvmCodec.newWasmBuffer(tx);
-        const result = SvmCodec.call("wasm_encode_call", buf);
-        const len = SvmCodec.wasmBufferLength(result);
-        const slice = SvmCodec.wasmBufferDataSlice(result, 0, len);
-        expect(slice[0]).toStrictEqual(SvmCodec.OK_MARKER);
-
-        SvmCodec.wasmBufferFree(buf);
-        SvmCodec.wasmBufferFree(result);
-
-        return slice.slice(1);
+        return SvmCodec.encodeCallData(data);
     }
 
-    function decodeCall(bytes: Uint8Array): JSON {
-        const data = binToString(bytes);
-        const buf = SvmCodec.newWasmBuffer({ data: data });
-        const result = SvmCodec.call("wasm_decode_call", buf);
-        const json = SvmCodec.loadWasmBufferDataAsJson(result);
-        SvmCodec.wasmBufferFree(buf);
-        SvmCodec.wasmBufferFree(result);
-
-        return json;
-    }
 
     it("Encodes & Decodes valid transaction", async function () {
 
         await init();
 
         const target = generateAddress("1020304050");
-        const verifydata = SvmCodec.encodeInput( {
+
+        const verifyData = SvmCodec.encodeInput( {
             abi: ["bool", "i8"],
             data: [true, 5],
         });
-        const calldata = SvmCodec.encodeInput( {
+
+        const callData = SvmCodec.encodeInput( {
             abi: ["i32", "i64"],
             data: [10, 20],
         });
-        const bytes = encodeCall(
+
+        const bytes = encodeCallData(
             target,
-            verifydata["data"],
-            calldata["data"]
+            verifyData["data"],
+            callData["data"]
         );
 
-        const json = decodeCall(bytes);
+        const json = SvmCodec.decodeCallData(bytes);
 
         expect(json).toStrictEqual({
             version: 0,
@@ -327,15 +257,15 @@ describe("Call Account", function () {
 
         await init();
 
-        const tx = { version: 0, target: "102030" };
-        const buf = SvmCodec.newWasmBuffer(tx);
-        const result = SvmCodec.call("wasm_encode_call", buf);
-        const error = SvmCodec.loadWasmBufferError(result);
+        expect(() => encodeCallData("102030", new Uint8Array(), new Uint8Array()))
+            .toThrow("The value of a specific field is invalid (`target`).");
+
+        /*
         expect(error).toStrictEqual(
             "The value of a specific field is invalid (`target`)."
-        );
+        );*/
 
-        SvmCodec.wasmBufferFree(buf);
-        SvmCodec.wasmBufferFree(result);
+        //SvmCodec.wasmBufferFree(buf);
+        //SvmCodec.wasmBufferFree(result);
     });
 });
